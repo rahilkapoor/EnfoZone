46b0c5eb4473daf122deaf20db96788ed3a7a577
Rahil Kapoor <rahilkapoor2000@gmail.com>
2020-07-28 15:29:00 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from posts.views import index, blog, post, search
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog, name='post-list'),
+    path('search/', search, name='search'),
+    path('post/<id>/', post, name='post-detail'),
+    path('tinymce/', include('tinymce.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
